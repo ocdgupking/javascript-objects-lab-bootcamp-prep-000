fea7eb3dc78a2087bcd27c88b08d7926824f2e4e
guptaco@ymail.com <guptaco@ymail.com>
2018-03-06 01:41:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = new Object()
 
 function updateObjectWithKeyAndValue(object, key, value){
   object[key] = value
-  return object
+  return object2
 }
 
 function updateObjectWithKeyAndValue(object, key, value){
