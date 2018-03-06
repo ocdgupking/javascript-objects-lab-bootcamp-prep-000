da3980de019224dbf186d1aaca324526d34abad5
guptaco@ymail.com <guptaco@ymail.com>
2018-03-06 02:23:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,4 +21,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 function deleteFromObjectByKey(object, key){
   var newObj = Object.assign({}, object)
   delete newObj.key
+  return newObj
 }
